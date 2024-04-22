1c27935172ede5fec1cce168b8ba51ace8493923
Ansahmbom <ansahmbomyong@gmail.com>
2024-04-22 19:08:26 +0200

    updated 3-tasks.py to meet pycodestyle requirements

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,11 +2,12 @@
 """This module implements the task_wait_random function
 which creates a task for the wait_random coroutine.
 """
-
 import asyncio
 from typing import Callable
 
+
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """Create a task for the wait_random coroutine with the given max_delay.
