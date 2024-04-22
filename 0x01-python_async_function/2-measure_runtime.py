df73d82821fc318260cd8b92a1dee9c32cebd2c1
Ansahmbom <ansahmbomyong@gmail.com>
2024-04-22 19:07:01 +0200

    added 3-tasks.py

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""This module implements the measure_time function
+which measures the total execution time for wait_n
+"""
+import asyncio
+import time
+import sys
+from typing import Callable
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """Measures the total execution time for wait_n
+    and returns total_time/n
+    
+    Args:
+        n (int): number of times to spawn wait_random
+        max_delay (int): maximum delay
+        
+    Returns:
+        float: total_time/n
+    """
+    start_time = time.time()
+    await wait_n(n, max_delay)
+    total_time = time.time() - start_time
+    return total_time / n
+
+
+async def main():
+    if len(sys.argv) != 3:
+        print("Usage: ./2-main.py <n> <max_delay>")
+        sys.exit(1)
+    result = await measure_time(int(sys.argv[1]), int(sys.argv[2]))
+    print(result)
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""This module implements the task_wait_random function
+which creates a task for the wait_random coroutine.
+"""
+
+import asyncio
+from typing import Callable
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Create a task for the wait_random coroutine with the given max_delay.
+
+    Args:
+        max_delay (int): The maximum delay for wait_random.
+
+    Returns:
+        asyncio.Task: A task for the wait_random coroutine.
+    """
+    return asyncio.create_task(wait_random(max_delay))
