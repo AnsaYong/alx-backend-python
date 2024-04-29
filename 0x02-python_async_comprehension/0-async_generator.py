caf7456d6e946e82bd0e8326beaa6abeca0650c9
Ansahmbom <ansahmbomyong@gmail.com>
2024-04-29 15:40:49 +0200

    updated 0-async_generator.py to ensure the function is type-annotated

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,9 +5,10 @@ no arguments.
 """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     This coroutine takes no input. It loops 10 times, waiting 1 sec after
     each loop and then generates a random number between 0 and 10.
