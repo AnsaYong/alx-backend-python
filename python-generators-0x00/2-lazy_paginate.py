56f5d97af0ea2a149d5e2cbfc590984d1ed4a830
Ansahmbom <ansahmbomyong@gmail.com>
2024-12-06 21:41:42 -0500

    2-lazy_paginate.py

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -2,7 +2,16 @@
 """
 This module contains a generator to lazily paginate users from the user_data table.
 """
-paginate_users = __import__("paginate_users").paginate_users
+seed = __import__("seed")
+
+
+def paginate_users(page_size, offset):
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
 
 
 def lazy_paginate(page_size):
