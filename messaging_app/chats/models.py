77cdecf983393d3e9275e5aae9d104c54d809cd5
Ansahmbom <ansahmbomyong@gmail.com>
2024-12-23 12:44:43 +0200

    added three fields to user model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -11,6 +11,9 @@ class user(AbstractUser):
     ]
 
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=30, null=False)
+    last_name = models.CharField(max_length=30, null=False)
+    password_harsh = models.CharField(max_length=128, null=False)
     phone_number = models.CharField(max_length=20, null=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
