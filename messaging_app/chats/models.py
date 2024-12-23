6e510e7f5f65774d36eec981f443c2179225ac3b
Ansahmbom <ansahmbomyong@gmail.com>
2024-12-23 12:38:23 +0200

    chats models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+
+class user(AbstractUser):
+    ROLE_CHOICES = [
+        ("guest", "Guest"),
+        ("host", "Host"),
+        ("admin", "Admin"),
+    ]
+
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    phone_number = models.CharField(max_length=20, null=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=["email"], name="unique_email")]
+        indexes = [
+            models.Index(fields=["email"], name="email_index"),
+        ]
+
+
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender_id = models.ForeignKey(
+        user.user_id, on_delete=models.CASCADE, related_name="sender"
+    )
+    message_body = models.TextField(null=False)
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
+    participants_id = models.ManyToManyField(user.user_id, related_name="participants")
+    created_at = models.DateTimeField(auto_now_add=True)
