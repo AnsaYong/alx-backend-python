8a69fb683c063fea688437f3a1669521e7ccd66d
Ansahmbom <ansahmbomyong@gmail.com>
2024-12-23 19:08:00 +0200

    updated chats/urls.py and messaging_app/urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -7,7 +7,14 @@ router = routers.DefaultRouter()
 router.register(r"conversations", ConversationViewSet, basename="conversation")
 router.register(r"messages", MessageViewSet, basename="message")
 
+# Nested Router
+nested_router = routers.NestedDefaultRouter(
+    router, r"conversations", lookup="conversation"
+)
+nested_router.register(r"messages", MessageViewSet, basename="conversation-messages")
+
 # URLs
 urlpatterns = [
     path("", include(router.urls)),
+    path("", include(nested_router.urls)),
 ]
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.urls import urlpatterns as auth_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("chats.urls")),
+    path("api-auth/", include(auth_urls)),  # Added for DRF's built-in auth endpoints
 ]
